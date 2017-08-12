4442285e0d4548a1d2d165f934c8ac3a2468029f
Philip <philm@manjaro.org>
2017-08-12 11:59:34 +0100

    [calamares-oem] add preinstall settings and modules

---
+++ calamares-oem/modules/oemuser/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# === This file is part of Calamares - <http://github.com/calamares> ===
+#
+#   Copyright 2017, Philip Müller <philm@manjaro.org>
+#
+#   Calamares is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Calamares is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+
+import libcalamares
+
+
+def run():
+    """ Set OEM User """
+
+    libcalamares.globalstorage.insert("autologinGroup", "autologin")
+    libcalamares.globalstorage.insert("autologinUser", "oem")
+    libcalamares.globalstorage.insert("hostname", "oem-pc")
+    libcalamares.globalstorage.insert("password", "oem")
+    libcalamares.globalstorage.insert("setRootPassword", "true")
+    libcalamares.globalstorage.insert("sudoersGroup", "wheel")
+    libcalamares.globalstorage.insert("username", "oem")
+
+    return None
